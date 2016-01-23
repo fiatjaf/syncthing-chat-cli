5c8d5004f52104ac4b61821961ba6ca4bc052338
fiatjaf@spooner <fiatjaf@gmail.com>
2016-01-23 12:39:35 -0500

    an usable version.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const chalk = require('chalk')
 const core = require('syncthing-chat-core')
+const action = core.action
+const event = core.event
+const d = core.data
 
 const helpers = require('./helpers')
 
@@ -12,60 +15,62 @@ const vorpal = require('vorpal')()
 vorpal.find('help').alias('?')
 vorpal.find('exit').description(`Exits syncthing-chat.`)
 
-var deviceNames = {}
-var devicesByName = {}
-core.listDevices().then(devices => {
-  devices.forEach(d => {
-    deviceNames[d.deviceID] = d.name
-    devicesByName[d.name] = d
-  })
-})
-
 vorpal
   .command('list devices')
   .alias('ls dev')
   .action(function (_, cb) {
-    core.listDevices().then(devs => {
-      this.log(devs.map(d => d.name).join('\n'))
-    }).then(cb)
+    action.listDevices()
+    .then(devs => this.log(devs.map(d => d.name).join('\n')))
+    .then(cb)
   })
 
 vorpal
   .command('list chats')
   .alias('ls chat', 'ls')
   .action(function (_, cb) {
-    core.listChats().then(chats => {
-      this.log(chats.map(c => deviceNames[c.deviceID]).join('\n'))
-    }).then(cb)
+    action.listDevicesWithChats()
+    .then(devs => this.log(devs.map(d => d.name).join('\n')))
+    .then(cb)
   })
 
 vorpal
   .command('create chat')
   .option('-w, --with <name>')
   .action(function (args, cb) {
-    let dev = devicesByName[args.options.with]
-    core.createChat(dev.deviceID)
-    .then(vorpal.exec('list chats'))
+    let dev = d.deviceByName[args.options.with]
+    action.createChat(dev.deviceID)
     .then(cb)
   })
 
 vorpal
-  .command('message <name> <message>')
+  .command('message <name> [message...]')
   .alias('/msg')
   .action(function (args, cb) {
-    let dev = devicesByName[args.name]
-    core.sendMessage(dev.deviceID, args.message)
+    let dev = d.deviceByName[args.name]
+    action.sendMessage(dev.deviceID, args.message.join(' '))
+    .then(cb)
+  })
+
+vorpal
+  .command('messages <name>')
+  .action(function (args, cb) {
+    let dev = d.deviceByName[args.name]
+    action.listMessages(dev.deviceID)
+    .then(msgs => this.log(msgs.map(m => `${helpers.stamp(m.time)} ${chalk.yellow(d.devices[m.sender].name)}: ${m.content}`).join('\n')))
+    .then(cb)
   })
 
 /* notify of events  */
-core.st.on('someoneOnline', (data) => {
-  console.log(`${helpers.stamp(data.time)} ${chalk.yellow(deviceNames[data.deviceID])} is online`)
+event.on('someoneOnline', (data) => {
+  vorpal.log(`${helpers.stamp(data.time)} ${chalk.yellow(d.devices[data.deviceID].name)} is online`)
 })
-core.st.on('someoneOffline', (data) => {
-  console.log(`${helpers.stamp(data.time)} ${chalk.yellow(deviceNames[data.deviceID])} is offline`)
+event.on('someoneOffline', (data) => {
+  vorpal.log(`${helpers.stamp(data.time)} ${chalk.yellow(d.devices[data.deviceID].name)} is offline`)
 })
-core.st.on('gotMessage', (data) => {
-  console.log(`${helpers.stamp(data.time)} ${chalk.yellow(deviceNames[data.deviceID])} sent: ${data.message}`)
+event.on('gotMessage', (data) => {
+  vorpal.log(`${helpers.stamp(data.time)} ${chalk.yellow(d.devices[data.deviceID].name)} sent: ${chalk.dim(data.message)}`)
 })
-
+event.on('chatAccepted', (data) => {
+  vorpal.log(`${helpers.stamp(data.time)} ${chalk.yellow(d.devices[data.deviceID].name)} has started a chat with you.`)
+})
 module.exports = vorpal
